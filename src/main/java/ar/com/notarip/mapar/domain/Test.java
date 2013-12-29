6a4f97cf073817fc629c24e2780c841ba73a2194
notarip <notarip@gmail.com>
2013-12-29 14:12:26 -0300

    First test class

---
+++ src/main/java/ar/com/notarip/mapar/domain/Test.java
@@ -0,0 +1,13 @@
+package ar.com.notarip.mapar.domain;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
